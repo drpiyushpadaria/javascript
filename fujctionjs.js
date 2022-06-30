5c038a988274cb74e14b9c08a4f985b8a947250e
drpiyushpadaria <drpiyush1687@gmail.com>
2022-06-30 15:34:32 +0530

    my work

---
+++ fujctionjs.js
@@ -0,0 +1,5 @@
+function myfunction(){
+    document.getElementById('demo').innerHTML="This is your HTML"}
+
+    function myfunction1(){
+        document.getElementById('demo').innerHTML="This is my web page"}
